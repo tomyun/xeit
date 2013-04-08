5dce48c4819113c9cc490874cfc6362da7fe7dc9
Kyungdahm Yun <tomyun@gmail.com>
2013-04-08 17:50:12 +0900

    Object literal 기반 module 패턴 적용

--- bootstrap/xeit.js
+++ bootstrap/xeit.js
@@ -1,6 +1,139 @@
-var Xeit = (function () {
-	var cls = function (attachment) {
-		this.init = function (attachment) {
+var xeit = (function () {
+	/********
+	 * Blob *
+	 ********/
+
+	var Blob = function (vendor) {
+		this.vendor = vendor || '';
+	};
+
+	Blob.prototype = {
+		encode: function (content) {
+			var message = CryptoJS.enc.Latin1.stringify(content);
+			if (message.match(/utf-8/i)) {
+				message = CryptoJS.enc.Utf8.stringify(content);
+			} else {
+				message = CryptoJS.enc.CP949.stringify(content);
+	        	message = message.replace(/euc-kr/ig, 'utf-8');
+			}
+
+			//HACK: DOCTYPE 없는 메일도 있으니 헤더와 본문 사이의 줄바꿈으로 인식.
+			// message = message.replace(/[\s\S]*(<!DOCTYPE)/i, "$1")
+			var offset = /[\n\r]{3,}/.exec(message);
+			if (offset) {
+				message = message.slice(offset.index);
+			}
+			return message;
+		}
+	};
+
+	/***************************
+	 * SoftForum XecureExpress *
+	 ***************************/
+
+	var XE = function (smime_header, smime_body, ui_desc) {
+		this.smime_header = smime_header || '';
+		this.smime_body = smime_body || '';
+		this.ui_desc = ui_desc || '';
+	};
+
+	XE.prototype = new Blob('xe');
+	$.extend(XE.prototype, {
+		decrypt: function (password) {
+			var headerWords = CryptoJS.enc.Base64.parse(this.smime_header);
+			var header = CryptoJS.enc.CP949.stringify(headerWords);
+
+			var contentType = header.match(/Content-Type: \s*([\w-\/]+);*/i)[1];
+			if (contentType == 'application/pkcs7-mime') {
+				var content = this.decryptSMIME(this.smime_body, password);
+			} else if (contentType == 'application/x-pwd') {
+				var key = header.match(/X-XE_KEY: \s*([\d]+): \s*([\w+\/=]+);*/i)[2];
+				var content = this.decryptPWD(key, this.smime_body, password);
+			}
+
+			// 메일 본문 인코딩 변환.
+			return this.encode(content);
+		},
+
+		decryptSMIME: function (envelope, password) {
+			var ciphers = {
+				desCBC: CryptoJS.DES,	// 1.3.14.3.2.7,
+				seedCBC: CryptoJS.SEED	// 1.2.410.200004.1.4
+			};
+
+			ASN1.prototype.contentRaw = function () {
+				var offset = this.posContent();
+				var length = this.length;
+				return this.stream.parseStringISO(offset, offset + length);
+			};
+
+			var der = Base64.unarmor(envelope);
+			var asn1 = ASN1.decode(der);
+			var envelopedData = asn1.sub[1].sub[0];
+
+			// 주민등록번호로 암호화된 대칭키 복호화.
+			var recipientInfos = envelopedData.sub[1];
+			var keyTransportRecipientInfo = recipientInfos.sub[0];
+			var keyEncryptionAlgorithm = oids[keyTransportRecipientInfo.sub[2].sub[0].content()].d;
+			var encryptedKey = CryptoJS.enc.Latin1.parse(keyTransportRecipientInfo.sub[3].contentRaw());
+			var passwordKey = CryptoJS.SHA1(password);
+			var iv = CryptoJS.enc.Hex.parse("0");
+			var decryptedKey = ciphers[keyEncryptionAlgorithm].decrypt(
+				{ ciphertext: encryptedKey },
+				passwordKey,
+				{ iv: iv }
+			);
+
+			// 대칭키로 암호화된 메일 본문 복호화.
+			var encryptedContentInfo = envelopedData.sub[2];
+			var contentEncryptionAlgorithm = oids[encryptedContentInfo.sub[1].sub[0].content()].d;
+			var encryptedContent = CryptoJS.enc.Latin1.parse(encryptedContentInfo.sub[2].contentRaw());
+			var decryptedContent = ciphers[contentEncryptionAlgorithm].decrypt(
+				{ ciphertext: encryptedContent },
+				decryptedKey,
+				{ iv: iv }
+			);
+			return decryptedContent;
+		},
+
+		decryptPWD: function (key, content, password) {
+			var encryptedKey = CryptoJS.enc.Base64.parse(key);
+			var passwordKey = CryptoJS.SHA1(password);
+			var iv = CryptoJS.enc.Hex.parse("0");
+			var decryptedKey = CryptoJS.SEED.decrypt(
+				{ ciphertext: encryptedKey },
+				passwordKey,
+				{ iv: iv }
+			);
+
+			var encryptedContent = CryptoJS.enc.Base64.parse(content);
+			var decryptedContent = CryptoJS.SEED.decrypt(
+				{ ciphertext: encryptedContent },
+				decryptedKey,
+				{ iv: iv }
+			);
+			return decryptedContent;
+		}
+	});
+
+	/************************
+	 * IniTech INISAFE Mail *
+	 ************************/
+
+	var IT = function (contents, attachedFile) {
+		this.contents = contents || '';
+		this.attachedFile = attachedFile || '';
+	};
+
+	IT.prototype = new Blob('it');
+	$.extend(IT.prototype, {
+		decrypt: function (password) {
+			alert('TODO: support IT');
+		}
+	});
+
+	return {
+		init: function (attachment) {
 			this.attachment = attachment || '';
 
 			if ($('#XEIViewer').length !== -1) {
@@ -21,9 +154,9 @@ var Xeit = (function () {
 			}
 
 			//TODO: updateBlob(blob)
-		};
+		},
 
-		this.updateAuthDialog = function () {
+		updateAuthDialog: function () {
 			var senders = {
 				TRUEFRIEND: { name: '한국투자증권', support: true },
 				'보안메일': { name: 'KB카드', support: true },
@@ -41,148 +174,10 @@ var Xeit = (function () {
 				$('#auth-password').prop('disabled', true);
 				$('#auth-submit').addClass('disabled');
 			}
-		};
+		},
 
-		this.decrypt = function (password) {
+		decrypt: function (password) {
 			return this.blob.decrypt(password);
-		};
-
-		/********
-		 * Blob *
-		 ********/
-
-		var Blob = function (vendor) {
-			this.vendor = vendor || '';
-		};
-
-		Blob.prototype = {
-			encode: function (content) {
-				var message = CryptoJS.enc.Latin1.stringify(content);
-				if (message.match(/utf-8/i)) {
-					message = CryptoJS.enc.Utf8.stringify(content);
-				} else {
-					message = CryptoJS.enc.CP949.stringify(content);
-		        	message = message.replace(/euc-kr/ig, 'utf-8');
-				}
-
-				//HACK: DOCTYPE 없는 메일도 있으니 헤더와 본문 사이의 줄바꿈으로 인식.
-				// message = message.replace(/[\s\S]*(<!DOCTYPE)/i, "$1")
-				var offset = /[\n\r]{3,}/.exec(message);
-				if (offset) {
-					message = message.slice(offset.index);
-				}
-				return message;
-			}
-		};
-
-		/***************************
-		 * SoftForum XecureExpress *
-		 ***************************/
-
-		var XE = function (smime_header, smime_body, ui_desc) {
-			this.smime_header = smime_header || '';
-			this.smime_body = smime_body || '';
-			this.ui_desc = ui_desc || '';
-		};
-
-		XE.prototype = new Blob('xe');
-		$.extend(XE.prototype, {
-			decrypt: function (password) {
-				var headerWords = CryptoJS.enc.Base64.parse(this.smime_header);
-				var header = CryptoJS.enc.CP949.stringify(headerWords);
-
-				var contentType = header.match(/Content-Type: \s*([\w-\/]+);*/i)[1];
-				if (contentType == 'application/pkcs7-mime') {
-					var content = this.decryptSMIME(this.smime_body, password);
-				} else if (contentType == 'application/x-pwd') {
-					var key = header.match(/X-XE_KEY: \s*([\d]+): \s*([\w+\/=]+);*/i)[2];
-					var content = this.decryptPWD(key, this.smime_body, password);
-				}
-
-				// 메일 본문 인코딩 변환.
-				return this.encode(content);
-			},
-
-			decryptSMIME: function (envelope, password) {
-				var ciphers = {
-					desCBC: CryptoJS.DES,	// 1.3.14.3.2.7,
-					seedCBC: CryptoJS.SEED	// 1.2.410.200004.1.4
-				};
-
-				ASN1.prototype.contentRaw = function () {
-					var offset = this.posContent();
-					var length = this.length;
-					return this.stream.parseStringISO(offset, offset + length);
-				};
-
-				var der = Base64.unarmor(envelope);
-				var asn1 = ASN1.decode(der);
-				var envelopedData = asn1.sub[1].sub[0];
-
-				// 주민등록번호로 암호화된 대칭키 복호화.
-				var recipientInfos = envelopedData.sub[1];
-				var keyTransportRecipientInfo = recipientInfos.sub[0];
-				var keyEncryptionAlgorithm = oids[keyTransportRecipientInfo.sub[2].sub[0].content()].d;
-				var encryptedKey = CryptoJS.enc.Latin1.parse(keyTransportRecipientInfo.sub[3].contentRaw());
-				var passwordKey = CryptoJS.SHA1(password);
-				var iv = CryptoJS.enc.Hex.parse("0");
-				var decryptedKey = ciphers[keyEncryptionAlgorithm].decrypt(
-					{ ciphertext: encryptedKey },
-					passwordKey,
-					{ iv: iv }
-				);
-
-				// 대칭키로 암호화된 메일 본문 복호화.
-				var encryptedContentInfo = envelopedData.sub[2];
-				var contentEncryptionAlgorithm = oids[encryptedContentInfo.sub[1].sub[0].content()].d;
-				var encryptedContent = CryptoJS.enc.Latin1.parse(encryptedContentInfo.sub[2].contentRaw());
-				var decryptedContent = ciphers[contentEncryptionAlgorithm].decrypt(
-					{ ciphertext: encryptedContent },
-					decryptedKey,
-					{ iv: iv }
-				);
-				return decryptedContent;
-			},
-
-			decryptPWD: function (key, content, password) {
-				var encryptedKey = CryptoJS.enc.Base64.parse(key);
-				var passwordKey = CryptoJS.SHA1(password);
-				var iv = CryptoJS.enc.Hex.parse("0");
-				var decryptedKey = CryptoJS.SEED.decrypt(
-					{ ciphertext: encryptedKey },
-					passwordKey,
-					{ iv: iv }
-				);
-
-				var encryptedContent = CryptoJS.enc.Base64.parse(content);
-				var decryptedContent = CryptoJS.SEED.decrypt(
-					{ ciphertext: encryptedContent },
-					decryptedKey,
-					{ iv: iv }
-				);
-				return decryptedContent;
-			}
-		});
-
-		/************************
-		 * IniTech INISAFE Mail *
-		 ************************/
-
-		var IT = function (contents, attachedFile) {
-			this.contents = contents || '';
-			this.attachedFile = attachedFile || '';
-		};
-
-		IT.prototype = new Blob('it');
-		$.extend(IT.prototype, {
-			decrypt: function (password) {
-				alert('TODO: support IT');
-			}
-		});
-
-		// constructor
-		this.init(attachment);
+		}
 	};
-
-	return cls;
 })();
