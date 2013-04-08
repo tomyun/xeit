a6af9b123b1d0df0bfefb2565eb90d8b5450059b
Kyungdahm Yun <tomyun@gmail.com>
2013-04-08 13:04:04 +0900

    이미 실행중이면 무시하

--- bookmarklet/xeit.js
+++ bookmarklet/xeit.js
@@ -1,11 +1,16 @@
 (function () {
+	var xeit = document.getElementById('xeit');
+	if (xeit) {
+		return;
+	}
+
 	var attachment = document.getElementsByTagName('body')[0].outerHTML;
 
 	document.body = document.createElement('body');
 	document.body.style.margin = 0;
 	document.body.style.padding = 0;
 
-	var xeit = document.createElement('iframe');
+	xeit = document.createElement('iframe');
 	xeit.setAttribute('id', 'xeit');
 	xeit.setAttribute('src', 'file:///Users/tomyun/Dropbox/SMIME/xeit/bootstrap/xeit.html');
 	xeit.style.width = '100%';
