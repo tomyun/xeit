5863578b08d2bf0cf77ce2d4cedfd0a54cffc947
Kyungdahm Yun <tomyun@gmail.com>
2013-04-08 15:31:05 +0900

    PKCS 복호화 알고리즘 코드 정리

--- bootstrap/xeit.js
+++ bootstrap/xeit.js
@@ -63,82 +63,62 @@ var Xeit = (function () {
 		};
 
 		this.decrypt_XE_PKCS = function (password) {
+			var ciphers = {
+				desCBC: CryptoJS.DES,	// 1.3.14.3.2.7,
+				seedCBC: CryptoJS.SEED	// 1.2.410.200004.1.4
+			};
+
+			ASN1.prototype.contentRaw = function () {
+				var offset = this.posContent();
+				var length = this.length;
+				return this.stream.parseStringISO(offset, offset + length);
+			};
+
 			var der = Base64.unarmor(this.blob.smime_body);
 			var asn1 = ASN1.decode(der);
-
-			/*
-			var p = document.createElement('P')
-			p.appendChild(asn1.toDOM())
-			document.body.appendChild(p)
-			*/
-
-			// pkcs7-envelopedData = 1.2.840.113549.1.7.3
-			var envelopedDataContentType = asn1.sub[0].content();
-	console.log(oids[envelopedDataContentType].d); //== 'envelopedData'
 			var envelopedData = asn1.sub[1].sub[0];
+
+			// 주민등록번호로 암호화된 대칭키 복호화.
 			var recipientInfos = envelopedData.sub[1];
 			var keyTransportRecipientInfo = recipientInfos.sub[0];
-			// des-cbc = 1.3.14.3.2.7
-			var keyEncryptionAlgorithm = keyTransportRecipientInfo.sub[2].sub[0].content();
-	console.log(oids[keyEncryptionAlgorithm].d); //== 'desCBC'
-			var encryptedKey = keyTransportRecipientInfo.sub[3].content();
+			var keyEncryptionAlgorithm = oids[keyTransportRecipientInfo.sub[2].sub[0].content()].d;
+			var encryptedKey = CryptoJS.enc.Latin1.parse(keyTransportRecipientInfo.sub[3].contentRaw());
+			var passwordKey = CryptoJS.SHA1(password);
+			var iv = CryptoJS.enc.Hex.parse("0");
+			var decryptedKey = ciphers[keyEncryptionAlgorithm].decrypt(
+				{ ciphertext: encryptedKey },
+				passwordKey,
+				{ iv: iv }
+			);
 
-			var c = keyTransportRecipientInfo.sub[3].posContent();
-			var l = keyTransportRecipientInfo.sub[3].length;
-			var w = keyTransportRecipientInfo.sub[3].stream.parseStringISO(c, c+l);
-			var W = CryptoJS.enc.Latin1.parse(w);
-
-			//var m = /\([\w\s]*\) (\w*)/.exec(encryptedKey);
-			//var w = CryptoJS.enc.Hex.parse(m[1]);
-
-			var ciphers = {
-				desCBC: CryptoJS.DES,
-				seedCBC: CryptoJS.SEED
-			}
-
-			var P = CryptoJS.SHA1(password);
-			var IV = CryptoJS.enc.Hex.parse("0");
-			//var K = CryptoJS.DES.decrypt({ciphertext: W}, P, {iv: IV});
-			//var K = CryptoJS.SEED.decrypt({ciphertext: W}, P, {iv: IV});
-			var K = ciphers[oids[keyEncryptionAlgorithm].d].decrypt({ciphertext: W}, P, {iv: IV});
-
-			console.log(K);
-			console.log(K.words);
-
+			// 대칭키로 암호화된 메일 본문 복호화.
 			var encryptedContentInfo = envelopedData.sub[2];
-			// pkcs7-data = 1.2.840.113549.1.7.1
-			var dataContentType = encryptedContentInfo.sub[0].content();
-	console.log(oids[dataContentType].d); //== 'data'
-			var contentEncryptionAlgorithm = encryptedContentInfo.sub[1].sub[0].content();
-	console.log(oids[contentEncryptionAlgorithm].d);
-			var encryptedContent = encryptedContentInfo.sub[2];
+			var contentEncryptionAlgorithm = oids[encryptedContentInfo.sub[1].sub[0].content()].d;
+			var encryptedContent = CryptoJS.enc.Latin1.parse(encryptedContentInfo.sub[2].contentRaw());
+			var decryptedContent = ciphers[contentEncryptionAlgorithm].decrypt(
+				{ ciphertext: encryptedContent },
+				decryptedKey,
+				{ iv: iv }
+			);
 
-			var c = encryptedContent.posContent();
-			var l = encryptedContent.length;
-			var s = encryptedContent.stream.parseStringISO(c, c+l);
-			var S = CryptoJS.enc.Latin1.parse(s);
-
-			//var R = CryptoJS.DES.decrypt({ciphertext: S}, K, {iv: IV});
-			//var R = CryptoJS.SEED.decrypt({ciphertext: S}, K, {iv: IV});
-			var R = ciphers[oids[contentEncryptionAlgorithm].d].decrypt({ciphertext: S}, K, {iv: IV});
-
-			var message = CryptoJS.enc.Latin1.stringify(R);
+			// 메일 본문 인코딩 변환.
+			var message = CryptoJS.enc.Latin1.stringify(decryptedContent);
 			if (message.match(/utf-8/i)) {
-				message = CryptoJS.enc.Utf8.stringify(R);
+				message = CryptoJS.enc.Utf8.stringify(decryptedContent);
 			} else {
-				message = CryptoJS.enc.CP949.stringify(R);
+				message = CryptoJS.enc.CP949.stringify(decryptedContent);
 	        	message = message.replace(/euc-kr/ig, 'utf-8');
 			}
 
 			//HACK: DOCTYPE 없는 메일도 있으니 헤더와 본문 사이의 줄바꿈으로 인식.
-			//var message = M.replace(/[\s\S]*(<!DOCTYPE)/i, "$1")
+			// message = message.replace(/[\s\S]*(<!DOCTYPE)/i, "$1")
 			var offset = /[\n\r]{3,}/.exec(message);
 			if (offset) {
 				message = message.slice(offset.index);
 			}
 			return message;
 		};
-		
+
 		this.init(attachment);
 	};
 
