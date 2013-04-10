e48fde7e78ccd89c70ffc80171864f6d9082bd2d
Kyungdahm Yun <tomyun@gmail.com>
2013-04-10 17:09:21 +0900

    탭 대신 공백으로 들여쓰기

--- bookmarklet/link.js
+++ bookmarklet/link.js
@@ -1,34 +1,34 @@
 (function () {
-	var xeit = document.getElementById('xeit');
-	if (xeit) {
-		return;
-	}
+    var xeit = document.getElementById('xeit');
+    if (xeit) {
+        return;
+    }
 
-	var attachment = document.getElementsByTagName('html')[0].outerHTML;
+    var attachment = document.getElementsByTagName('html')[0].outerHTML;
 
-	var _body = document.body;
-	document.body = document.createElement('body');
-	document.body.style.margin = 0;
-	document.body.style.padding = 0;
-	document.body.style.height = '100%';
+    var _body = document.body;
+    document.body = document.createElement('body');
+    document.body.style.margin = 0;
+    document.body.style.padding = 0;
+    document.body.style.height = '100%';
 
-	xeit = document.createElement('iframe');
-	xeit.setAttribute('id', 'xeit');
-	xeit.setAttribute('src', 'file:///Users/tomyun/Dropbox/SMIME/xeit/bootstrap/xeit.html');
-	xeit.style.width = '100%';
-	xeit.style.height = '100%';
-	xeit.style.border = 0;
-	document.body.appendChild(xeit);
+    xeit = document.createElement('iframe');
+    xeit.setAttribute('id', 'xeit');
+    xeit.setAttribute('src', 'file:///Users/tomyun/Dropbox/SMIME/xeit/bootstrap/xeit.html');
+    xeit.style.width = '100%';
+    xeit.style.height = '100%';
+    xeit.style.border = 0;
+    document.body.appendChild(xeit);
 
-	window.addEventListener('message', function (e) {
-		switch (e.data) {
-			case 'ready':
-			xeit.contentWindow.postMessage(attachment, '*');
-			break;
+    window.addEventListener('message', function (e) {
+        switch (e.data) {
+            case 'ready':
+            xeit.contentWindow.postMessage(attachment, '*');
+            break;
 
-			case 'fallback':
-			document.body = _body;
-			break;
-		}
-	}, false);
+            case 'fallback':
+            document.body = _body;
+            break;
+        }
+    }, false);
 })();
