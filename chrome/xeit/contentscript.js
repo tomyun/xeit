bc63de8232865456b3aac105fe097fd2a95ef5ee
Kyungdahm Yun <tomyun@gmail.com>
2013-06-16 00:39:05 +0900

    Shorten bookmarklet script

--- chrome/xeit/contentscript.js
+++ chrome/xeit/contentscript.js
@@ -9,41 +9,33 @@ function check() {
 
 // (almost) copy of link.js
 function link() {
-    var xeit = document.getElementById('xeit');
+    var doc = document;
+    var xeit = doc.getElementById('xeit');
     if (xeit) {
         return;
     }
 
-    window.stop();
-    var attachment = document.getElementsByTagName('html')[0].outerHTML;
-
-    document.documentElement.style.height = '100%';
-
-    var _body = document.body;
-    document.body = document.createElement('body');
-    document.body.style.margin = 0;
-    document.body.style.padding = 0;
-    document.body.style.height = '100%';
-
-    xeit = document.createElement('iframe');
-    xeit.setAttribute('id', 'xeit');
-    xeit.setAttribute('src', 'http://tomyun.github.io/xeit/xeit.html?chrome');
-    xeit.style.width = '100%';
-    xeit.style.height = '100%';
-    xeit.style.border = 0;
-    document.body.appendChild(xeit);
-
     window.addEventListener('message', function (e) {
-        switch (e.data) {
-            case 'ready':
+        if (e.data == 'ready') {
             xeit.contentWindow.postMessage(attachment, '*');
-            break;
-
-            case 'fallback':
-            document.body = _body;
-            break;
+        } else if (e.data == 'fallback') {
+            doc.body = _body;
         }
-    }, false);
+    });
+
+    var html = doc.documentElement;
+    var attachment = html.outerHTML;
+    html.style.height = '100%';
+
+    var _body = doc.body;
+    var body = doc.body = doc.createElement('body');
+    body.style.cssText = 'margin:0;padding:0;height:100%';
+
+    xeit = doc.createElement('iframe');
+    xeit.id = 'xeit';
+    xeit.src = 'http://tomyun.github.io/xeit/xeit.html' + '?chrome';
+    xeit.style.cssText = 'width:100%;height:100%;border:0';
+    body.appendChild(xeit);
 }
 
 function show() {
