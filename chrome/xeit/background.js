9d7f8db5b53684ee87c8ceb05ca8f456e5760fdd
Kyungdahm Yun <tomyun@gmail.com>
2013-07-20 00:24:15 +0900

    Allow fallback to original page using pageAction icon

--- chrome/xeit/background.js
+++ chrome/xeit/background.js
@@ -2,3 +2,9 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
     chrome.pageAction.show(sender.tab.id);
     sendResponse({});
 });
+
+chrome.pageAction.onClicked.addListener(function (tab) {
+    chrome.tabs.sendMessage(tab.id, 'fallback', function (response) {
+        chrome.pageAction.hide(tab.id);
+    });
+});
